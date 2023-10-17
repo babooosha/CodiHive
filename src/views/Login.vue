a44d93ee02bd48a72edf42eeb548442b8d9a8431
babooosha <bajeronimohenriquesrocha@gmail.com>
2023-10-17 22:38:33 +0100

    resolved login and sign-up position

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,7 +8,7 @@
        </p>
     </div> -->
     
-    <div class="bg-grey-lighter min-h-screen flex flex-col">
+    <div class="bg-grey-lighter min-h-screen w-screen flex flex-col">
             <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 class="mb-8 text-3xl font-bold leading-6 text-gray-700 text-center">Login</h1>
